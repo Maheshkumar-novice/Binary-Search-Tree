34207e5130b2e87901f4e551366d37f686b8a202
Maheshkumar <callmeelprofessor@gmail.com>
2021-09-25 15:57:37 +0530

    Basic File Structure

---
+++ binary-search-tree.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Binary Search Tree
+class Tree
+  def initialize; end
+end
---
+++ tree-node.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Tree Node
+class Node
+  def initialize; end
+end
