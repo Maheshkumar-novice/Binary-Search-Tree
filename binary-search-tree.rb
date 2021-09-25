13497324bd1a153554ddca48ecc0b4d62ba59043
Maheshkumar <callmeelprofessor@gmail.com>
2021-09-25 17:18:44 +0530

    Add tree builder

--- binary-search-tree.rb
+++ binary-search-tree.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require_relative 'tree-node'
+
 # Binary Search Tree
 class Tree
   def initialize(array)
@@ -9,6 +11,10 @@ class Tree
 
   def build_tree(array)
     array = prepare_the_array(array)
+
+    start_index = 0
+    end_index = array.size - 1
+    create_bst(start_index, end_index, array)
   end
 
   private
@@ -24,5 +30,15 @@ class Tree
 
   def sort_the_array(array)
     array.sort
+  end
+
+  def create_bst(start_index, end_index, array)
+    return nil if start_index > end_index
+
+    middle_index = (start_index + end_index) / 2
+    root = Node.new(array[middle_index])
+    root.left = create_bst(start_index, middle_index - 1, array)
+    root.right = create_bst(middle_index + 1, end_index, array)
+    root
   end
 end
