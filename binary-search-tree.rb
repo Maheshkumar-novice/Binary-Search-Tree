0775a6849c7dcd52ecdd38a697fd06d2884c12ca
Maheshkumar <callmeelprofessor@gmail.com>
2021-09-27 11:43:00 +0530

    Complete naive #delete

--- binary-search-tree.rb
+++ binary-search-tree.rb
@@ -39,33 +39,62 @@ class Tree
   end
 
   def delete(value, node = @root)
+    if @root.data == value && node == @root
+      # binding.pry
+      successor, parent_of_successor = find_succ(@root.right, @root.right)
+      node.data = successor.data
+      delete(successor.data, parent_of_successor)
+      return
+    end
+    # binding.pry
     if value < node.data
       if node.left.data == value
-        return node.left = nil if node.left.right.nil? && node.left.left.nil?
-
-        if node.left.right.nil? || node.left.left.nil?
+        if node.left.right.nil? && node.left.left.nil?
+          node.left = nil
+        elsif node.left.right.nil? || node.left.left.nil?
           remove = [node.left.right, node.left.left].find { |x| !x.nil? }
           node.left.data = remove.data
-          node.left.left = nil
-          node.left.right = nil
+          node.left.left = remove.left
+          node.left.right = remove.right
+        else
+          successor, parent_of_successor = find_succ(node.left.right, node.left)
+          node.left.data = successor.data
+          delete(successor.data, parent_of_successor)
         end
       else
         delete(value, node.left)
       end
-    elsif value > node.data
-      if node.right.data == value
-        return node.right = nil if node.right.right.nil? && node.right.left.nil?
+    elsif value >= node.data
+      # binding.pry
 
-        if node.right.right.nil? || node.right.left.nil?
+      if node.right.data == value
+        if node.right.right.nil? && node.right.left.nil?
+          node.right = nil
+        elsif node.right.right.nil? || node.right.left.nil?
           remove = [node.right.right, node.right.left].find { |x| !x.nil? }
           node.right.data = remove.data
-          node.right.left = nil
-          node.right.right = nil
+          node.right.left = remove.left
+          node.right.right = remove.right
+        else
+          # binding.pry
+          successor, parent_of_successor = find_succ(node.right.right, node.right)
+          node.right.data = successor.data
+          delete(successor.data, parent_of_successor)
         end
       else
         delete(value, node.right)
       end
     end
+  end
+
+  def find_succ(node, parent)
+    # binding.pry
+    return [node, parent] if node.right.nil? && node.left.nil?
+    return [node, parent] if node.left.nil?
+
+    # return [node, parent] if node.right.nil?
+
+    find_succ(node.left, node)
   end
 
   def to_s
