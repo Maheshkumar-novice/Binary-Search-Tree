60c19151f2bf783d3b28eb92bcef4644ba2892fb
Maheshkumar <callmeelprofessor@gmail.com>
2021-09-30 09:50:01 +0530

    Include Comparable

--- tree-node.rb
+++ tree-node.rb
@@ -3,11 +3,23 @@
 
 # Tree Node
 class Node
+  include Comparable
+
   attr_accessor :left, :right, :data
 
   def initialize(data, left = nil, right = nil)
     @data = data
     @left = left
     @right = right
+  end
+
+  def to_s
+    @data.to_s
+  end
+
+  def <=>(other)
+    other_value = other.instance_of?(Node) ? other.data : other
+
+    @data <=> other_value
   end
 end
